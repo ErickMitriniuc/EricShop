437f7a57776a6891acf2b8d944ebceeceb1ab62b
ErickMitriniuc <44193180+ErickMitriniuc@users.noreply.github.com>
2020-04-23 13:42:10 +0300

    Add files via upload

---
+++ EricShopVue/src/components/main-page/v-main-page.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='v-main-page'>
+    <h1>ShopEric</h1>
+    <p>
+      <router-link :to="{name: 'catalog'}">
+        <h1>Каталог</h1>
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "v-main-page",
+    props: {},
+    data() {
+      return {}
+    },
+    computed: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ EricShopVue/src/components/popup/v-popup.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="popup_wrapper" ref="popup_wrapper">
+        <div class='v-popup'>
+            <div class="v-popup__header">
+                <span>{{popupTitle}}</span>
+                <span>
+        <i
+                class="material-icons"
+                @click="closePopup"
+        >
+          закрыть
+        </i>
+      </span>
+            </div>
+            <div class="v-popup__content">
+                <slot></slot>
+            </div>
+            <div class="v-popup__footer">
+                <button class="close_modal" @click="closePopup">Закрыть</button>
+                <button
+                        class="submit_btn"
+                        @click="rightBtnAction"
+                >
+                    Добавить
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "v-popup",
+        props: {
+            popupTitle: {
+                type: String,
+                default: 'Popup name'
+            },
+            rightBtnTitle: {
+                type: String,
+                default: 'Добавить'
+            }
+        },
+        data() {
+            return {}
+        },
+        methods: {
+            rightBtnAction() {
+                this.$emit('rightBtnAction')
+            },
+            closePopup() {
+                this.$emit('closePopup')
+            }
+        },
+        mounted() {
+            let vm = this;
+            document.addEventListener('click', function (item) {
+                if (item.target === vm.$refs['popup_wrapper']) {
+                    vm.closePopup()
+                }
+            })
+        },
+    }
+</script>
+
+<style lang="scss">
+    .popup_wrapper {
+        background: rgba(64, 64, 64, .4);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        right: 0;
+        left: 0;
+        top: 0;
+        bottom: 0;
+    }
+
+    .v-popup {
+        padding: 16px;
+        position: fixed;
+        top: 50px;
+        width: 400px;
+        background: #ffffff;
+        box-shadow: 0 0 17px 0 #d9044f;
+        z-index: 10;
+
+        &__header, &__footer {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        &__content {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .submit_btn {
+            padding: 8px;
+            color: #ffffff;
+            background: #ef276f;
+        }
+
+        .close_modal {
+            padding: 8px;
+            color: #ffffff;
+            background: #3882a9;
+        }
+    }
+</style>
